e3ddf44eae2f00cc2345a428fd3c7e01f3b3be74
Mostafa Hamed <36406025+MostafaHamedAbdelmasoud@users.noreply.github.com>
2020-02-16 13:03:05 -0800

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 ob_start();
 
 try {
-	heroku_395f1ee48aedde5
+	//heroku_395f1ee48aedde5
 	//$con = new PDO("mysql:dbname=doodle;host=localhost", "root", "");
 	$con = new PDO("mysql:dbname=heroku_395f1ee48aedde5;host=us-cdbr-iron-east-04.cleardb.net", "b41d7bab336c8b", "7a7f5fcf");
 	
